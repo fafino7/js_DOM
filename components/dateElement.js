d5aa34aef10d58ed9a590dc1429d5acf1f4754d8
fafino7 <fafierro.o@gmail.com>
2022-06-09 19:30:26 -0400

    últimas correcciones en para github pages

--- components/dateElement.js
+++ components/dateElement.js
@@ -1,14 +1,6 @@
 export default (date) => {
-<<<<<<< HEAD
-  const dateElement = document.createElement('li');
-  dateElement.classList.add('date');
-  dateElement.innerHTML = date;
-  return dateElement;
-};
-=======
     const dateElement = document.createElement('li');
     dateElement.classList.add('date');
     dateElement.innerHTML = date;
     return dateElement;
   };
->>>>>>> projectoInicial
--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-import { addTask } from './components/addTask.js';
 import { addTask } from "./components/addTask.js";
 import { displayTasks } from './components/readTasks.js';
 
 const btn = document.querySelector('[data-form-btn]');
 btn.addEventListener('click', addTask);
 
-//Arrow functions o funciones anonimas
-btn.addEventListener('click', addTask);
-
 displayTasks();
-displayTasks();
-
-//projectoInicial
